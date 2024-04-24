637a07f8a00ea7f44d4dab8c9b093c00a7e4161d
Souvik Kanungo <sooovik2@gmail.com>
2024-04-24 13:28:18 +0530

    msg

--- src/InfoBox.jsx
+++ src/InfoBox.jsx
@@ -3,10 +3,19 @@ import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
 import "./InfoBox.css";
+import AcUnitIcon from "@mui/icons-material/AcUnit";
+import ThunderstormIcon from "@mui/icons-material/Thunderstorm";
+import WbSunnyIcon from "@mui/icons-material/WbSunny";
 
 export default function InfoBox(info) {
   const INIT_URL =
-    "https://images.unsplash.com/photo-1567329799309-4df7e4782811?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
+    "https://plus.unsplash.com/premium_photo-1673081112888-f877c594ad18?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
+  const HOT_URL =
+    "https://images.unsplash.com/uploads/14121010130570e22bcdf/e1730efe?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
+  const COLD_URL =
+    "https://images.unsplash.com/photo-1616951849649-74dd2dd7e662?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
+  const RAIN_URL =
+    "https://plus.unsplash.com/premium_photo-1666258224619-a018ae456f86?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
 
   return (
     <div className="Infobox">
@@ -14,12 +23,23 @@ export default function InfoBox(info) {
         <Card sx={{ maxWidth: 345 }}>
           <CardMedia
             sx={{ height: 140 }}
-            image={INIT_URL}
+            image={
+              info.humidity > 80
+                ? RAIN_URL
+                : info.temp > 15
+                ? WbSunnyIcon
+                : AcUnitIcon
+            }
             title="green iguana"
           />
           <CardContent>
             <Typography gutterBottom variant="h5" component="div">
               {info.city}
+              {info.humidity > 80
+                ? ThunderstormIcon
+                : info.temp > 15
+                ? HOT_URL
+                : COLD_URL}
             </Typography>
             <Typography
               variant="body2"
--- src/WeatherApp.jsx
+++ src/WeatherApp.jsx
@@ -1,3 +1,4 @@
+import React, { useState } from "react";
 import SearchBox from "./SearchBox";
 import InfoBox from "./InfoBox";
 
